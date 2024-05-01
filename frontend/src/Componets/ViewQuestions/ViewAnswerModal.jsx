b63e0b419a7a3993782801ea3f63b402834df39a
kareena-29 <kareenathakrel2002@gmail.com>
2024-05-01 10:37:38 +0530

    questions crud

--- frontend/src/Componets/ViewQuestions/ViewAnswerModal.jsx
+++ frontend/src/Componets/ViewQuestions/ViewAnswerModal.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./viewAnswerModal.css";
-
-const ViewAnswerModal = ({isOpen, onClose}) => {
+import Grid from "@mui/material/Grid";
+const ViewAnswerModal = ({ isOpen, onClose, answers }) => {
+  console.log(answers)
+  const [answerList, setAnswerList] = useState(answers);
   return (
     <>
       {isOpen && (
@@ -8,7 +11,29 @@ const ViewAnswerModal = ({isOpen, onClose}) => {
           <div className="closingModalDiv" onClick={onClose}>
             <span> &times;</span>
           </div>
-          ANS
+          <div className="answertitle">
+            Possible Answers
+          </div>
+          <div className="answerSection">
+            <table className="answerContentTable">
+              <thead>
+                <tr>
+                  <th>Answer Value</th>
+                  <th>Is Quantifiable</th>
+                  <th>Cost</th>
+                </tr>
+              </thead>
+              <tbody>
+                {answerList.map((ans, index) => (
+                  <tr key={index}>
+                    <td>{ans.answerValue}</td>
+                    <td>{`${ans.isQuantifyable}`}</td>
+                    <td>{ans.cost}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       )}
     </>
